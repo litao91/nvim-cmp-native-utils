0e88e93f0d455f9f472e421073b18fde759b6140
tl <>
2022-02-08 11:54:18 +0800

    Update

--- src/entry.rs
+++ src/entry.rs
@@ -23,13 +23,36 @@ pub fn get_completion_item<'lua>(entry: &LuaTable<'lua>) -> LuaResult<LuaTable<'
 
 pub static INSERT_TEXT_FORMAT_SNIPPET: i32 = 2;
 
-pub fn get_override(text_edit: &TextEdit, cursor_line: &str) -> LuaResult<(i32, i32)> {
-    todo!()
+pub fn get_override(
+    entry: &LuaTable,
+    text_edit: &TextEdit,
+    cursor_line: &str,
+) -> LuaResult<(i32, i32)> {
+    let r = if let Some(insert) = &text_edit.insert {
+        Some(insert.clone())
+    } else {
+        if let Some(range) = &text_edit.range {
+            Some(range.clone())
+        } else {
+            None
+        }
+    }
+    .unwrap();
+    let s = misc::to_vimindex(&cursor_line, r.start.character as usize) as i32;
+    let e = misc::to_vimindex(&cursor_line, r.end.character as usize) as i32;
+    let col = entry
+        .get::<_, LuaTable>("context")?
+        .get::<_, LuaTable>("cursor")?
+        .get::<_, i32>("col")?;
+    let before = col - s;
+    let after = e - col;
+    Ok((before, after))
 }
 
-pub fn get_word<'lua>(
-    completion_item: &LuaTable<'lua>,
-    text_edit: &'lua Option<TextEdit>,
+pub fn get_word(
+    entry: &LuaTable,
+    completion_item: &LuaTable,
+    text_edit: &Option<TextEdit>,
     cursor_line: &str,
 ) -> LuaResult<String> {
     let mut word;
@@ -43,7 +66,7 @@ pub fn get_word<'lua>(
 
     if let Some(text_edit) = text_edit {
         word = text_edit.new_text.trim();
-        let override_range = get_override(text_edit, cursor_line)?;
+        let override_range = get_override(entry, text_edit, cursor_line)?;
         if 0 < override_range.1 || insert_text_format == INSERT_TEXT_FORMAT_SNIPPET {
             word = str_utils::get_word(&word, 0);
         }
